ca21482a1dea9a5c3f863c270e49dc91dbd9812f
Mfoniso Fabian <goodlucketim94@gmail.com>
2023-01-24 12:59:17 +0100

    update

--- js/index.js
+++ js/index.js
@@ -47,24 +47,73 @@ const swiper = new Swiper('.swiper', {
   const btn = document.querySelector('.menu-btn')
   const navBtn = document.querySelector('.nav-btn')
 
-  btn.onclick = function(){
-    let menu = document.querySelector('.menu-links')
+  // btn.onclick = function(){
+  //   let menu = document.querySelector('.menu-links')
 
-    if(menu.style.display=="block"){
-      menu.style.display = 'none'
-    }else{
-      menu.style.display = 'block'
-    }
+  //   if(menu.style.display=="block"){
+  //     menu.style.display = 'none'
+  //   }else{
+  //     menu.style.display = 'block'
+  //   }
+  // }
+
+  // navBtn.onclick = function() {
+  //   let menu = document.querySelector('.menu-links')
+  //   console.log('clicked');
+  //   if(menu.style.display=="block"){
+  //     menu.style.display = 'none'
+  //   }else{
+  //     menu.style.display = 'block'
+  //     // menu.style.width = '100%'
+  //     menu.style.background = 'white'
+  //   }
+  // }
+
+  const accordionShw = document.querySelector('.show')
+  const accordionHide = document.querySelector('.hide')
+  const accordionShw1 = document.querySelector('.show1')
+  const accordionHide1 = document.querySelector('.hide1')
+  const accordionShw2 = document.querySelector('.show2')
+  const accordionHide2 = document.querySelector('.hide2')
+
+  accordionShw.onclick = function (){
+    let accordTxt = document.querySelector('.accordion-txt')
+    accordTxt.style.display = 'block'
+    accordionHide.style.display = 'block'
+    accordionShw.style.display = 'none'
   }
 
-  navBtn.onclick = function() {
-    let menu = document.querySelector('.menu-links')
-    console.log('clicked');
-    if(menu.style.display=="block"){
-      menu.style.display = 'none'
-    }else{
-      menu.style.display = 'block'
-      // menu.style.width = '100%'
-      menu.style.background = 'white'
-    }
+  accordionHide.onclick = function (){
+    let accordTxt = document.querySelector('.accordion-txt')
+    accordTxt.style.display = 'none'
+    accordionHide.style.display = 'none'
+    accordionShw.style.display = 'block'
+  }
+
+  accordionShw1.onclick = function (){
+    let accordTxt = document.querySelector('.accordion-txt1')
+    accordTxt.style.display = 'block'
+    accordionHide1.style.display = 'block'
+    accordionShw1.style.display = 'none'
+  }
+
+  accordionHide1.onclick = function (){
+    let accordTxt = document.querySelector('.accordion-txt1')
+    accordTxt.style.display = 'none'
+    accordionHide1.style.display = 'none'
+    accordionShw1.style.display = 'block'
+  }
+
+  accordionShw2.onclick = function (){
+    let accordTxt = document.querySelector('.accordion-txt2')
+    accordTxt.style.display = 'block'
+    accordionHide2.style.display = 'block'
+    accordionShw2.style.display = 'none'
+  }
+
+  accordionHide2.onclick = function (){
+    let accordTxt = document.querySelector('.accordion-txt2')
+    accordTxt.style.display = 'none'
+    accordionHide2.style.display = 'none'
+    accordionShw2.style.display = 'block'
   }
